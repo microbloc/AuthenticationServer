18fbd38fbafa5f91896cdeee06f084ea4e098e8b
Mohit Acharya <mohitacharya4@gmail.com>
2019-04-22 21:30:15 +0530

    #1-CreateAuthServer: initial commit | setting up the auth server

---
+++ src/main/java/com/microbloc/accessors/Accessor.java
@@ -0,0 +1,13 @@
+package com.microbloc.accessors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accessor {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accessor.class, args);
+	}
+
+}
